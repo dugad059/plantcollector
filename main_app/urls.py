3dc6cecd60c201ddcb92a8e2edd53f6995955851
David Dugas <daviddugas@Davids-MacBook-Pro.local>
2022-02-17 19:23:33 -0600

    wip: one to many complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('plants/create/', views.PlantCreate.as_view(), name='create_plants'),
     path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='update_plants'),
     path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='delete_plants'),
+    path('plants/<int:plant_id>/add_watering', views.add_watering, name='add_watering'),
 ]   
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,26 @@
-from dataclasses import field
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Plant
+from .forms import WateringForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
+def add_watering(request, plant_id):
+    form = WateringForm(request.POST)
+    if form.is_valid():
+        new_watering = form.save(commit=False)
+        new_watering.plant_id = plant_id
+        new_watering.save()
+    return redirect('detail_plants', plant_id=plant_id)
+
 
 
 """
@@ -18,25 +30,31 @@ def about(request):
 
 """
 
+
 def plants_index(request):
     plants = Plant.objects.all()
-    return render(request, 'plants/index.html', {'plants' : plants})
+    return render(request, 'plants/index.html', {'plants': plants})
+
 
 def plants_detail(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
-    return render(request, 'plants/detail.html', { 'plant' : plant })
+    watering_form = WateringForm()
+    return render(request, 'plants/detail.html',
+                  {'plant': plant,
+                  'watering_form': watering_form}
+                  )
+
 
 class PlantCreate(CreateView):
     model = Plant
     fields = '__all__'
 
+
 class PlantUpdate(UpdateView):
     model = Plant
     fields = ('name', 'waterings', 'sunlight', 'description')
 
+
 class PlantDelete(DeleteView):
     model = Plant
     success_url = '/plants/'
-
-
-
